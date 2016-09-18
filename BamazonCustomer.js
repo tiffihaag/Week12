bfad2159e0e00dddd8ff142828320e03dd2eee91
tiffihaag <tiffihaag@gmail.com>
2016-09-17 22:50:06 -0400

    it was almost there

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -1,5 +1,12 @@
 var db = require('./connection');
-var prompt = require('./db');
+var fs = require('fs');
+
+var pick = null;
+var productsValue = []; //push array values here
+var productsQty = []; //push array values here
+var counter = 0;
+var itemPicked;
+var qtyPicked;
 
 //Database query 
 db.query('SELECT * FROM products', function (error, results, fields){
@@ -10,7 +17,12 @@ db.query('SELECT * FROM products', function (error, results, fields){
 	}
 	results.forEach(function(value) {
 		console.log(value.ProductName + " | Price: $ " + value.Price + " | Qty: " + value.StockQuantity);
-	});
+		
+		productsValue[counter] = value.ProductName;
+		productsQty[counter] = value.productsQty;
+		counter = counter + 1;
+	}); //for value function
+}); //for query
 
 	// Questions
 	var inquire = require('inquirer');
@@ -18,37 +30,40 @@ db.query('SELECT * FROM products', function (error, results, fields){
 	{
 	  type: "input",
 	  name: "pickItem",
-	  message: "Which item would you like to purchase from the list above?",
-	  choices: ["Paper Towels", "Toilet Paper", "Hand Soap", "Deodorant", "Diapers", "Wipes", "Kitchen Towels", "Pizza Pan", "Camera", "Headphones"]
+	  message: "Which item would you like to purchase from the list below?",
+	  choices: [productsValue]
 	}
 	]).then(function (user) {
 	  console.log("You chose: " + user.pickItem);
 	  var itemPicked = user.pickItem;
-	}); //for inquirer function
-
-	inquire.prompt([
-	{
-	  type: "input",
-	  message: "How many would you like to purchase?",
-	  name: "pickQty"
-	}
-	]).then(function (user) {
-	  console.log("You selected: " user.pickQty);
-	  var qtyPicked = user.pickQty;
 	  
-	  if (item quantity <= value.StockQuantity) {
+		inquire.prompt([
+		{
+		  type: "input",
+		  message: "How many would you like to purchase?",
+		  name: "pickQty"
+		}
+		]).then(function (user) {
+		  console.log("You selected: " + user.pickQty + " of " + itemPicked);
+		  var qtyPicked = user.pickQty;
+		  }); //for user function
+		}); //for inquirer value function
 
-	  }
+  	for (var i = 0; i < productsValue.length; i++) {
+  		if (itemPicked == productsValue[i] && qtyPicked <= productsQty[i]) {
+  			console.log("Looks like we have the item(s) you're looking for.");
+  		};
+  	}; 
 
 	//upate the database of purchase
-	// connection.query("UPDATE products SET itemPicked WHERE ?"), [{
-	// 	ProductName: user.pickItem,
-	// 	DepartmentName: value.DepartmentName,
-	// 	Price: value.Price,
-	// 	StockQuantity: value.StockQuantity - user.pickQty 
-	// }], function(err, res) {});
-	
-	}); //for second question function
+	db.query('UPDATE products SET ? WHERE ?', [{
+		StockQuantity: productsQty[i] - qtyPicked 
+		},
+		{
+			ProductName: itemPicked 
+		}], function(err, res) {
+			console.log("You've successfully placed your order.");
+		});
 
-}); //for query
+
 
